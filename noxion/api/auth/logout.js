2061b003800731c6d26efb73aac38567baecddbc
tiiiitktok <samiraaugusto12@gmail.com>
2026-08-08 19:30:59 -0300

    Delete noxion directory

--- noxion/api/auth/logout.js
+++
@@ -1,9 +0,0 @@
-const { clearSessionCookie } = require("../../lib/auth");
-
-module.exports = async (req, res) => {
-  if (req.method !== "POST") {
-    return res.status(405).json({ ok: false, erro: "método não permitido" });
-  }
-  clearSessionCookie(req, res);
-  res.status(200).json({ ok: true });
-};
